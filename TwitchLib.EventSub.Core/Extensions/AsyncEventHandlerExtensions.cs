c89d257495fc2c734004e9164d6d5adc126c9fec
AoshiW <Aoshi.W@gmail.com>
2025-05-16 13:11:02 +0200

    fix nullable annotation, add default value for non-nullable props

--- TwitchLib.EventSub.Core/Extensions/AsyncEventHandlerExtensions.cs
+++ TwitchLib.EventSub.Core/Extensions/AsyncEventHandlerExtensions.cs
@@ -5,12 +5,12 @@ namespace TwitchLib.EventSub.Core.Extensions;
 
 public static class AsyncEventHandlerExtensions
 {
-    public static Task InvokeAsync<TEventArgs>(this AsyncEventHandler<TEventArgs> asyncEventHandler, object sender, TEventArgs args)
+    public static Task InvokeAsync<TEventArgs>(this AsyncEventHandler<TEventArgs>? asyncEventHandler, object sender, TEventArgs args)
     {
         return asyncEventHandler != null ? asyncEventHandler(sender, args) : Task.CompletedTask;
     }
         
-    public static Task InvokeAsync(this AsyncEventHandler asyncEventHandler, object sender, EventArgs args)
+    public static Task InvokeAsync(this AsyncEventHandler? asyncEventHandler, object sender, EventArgs args)
     {
         return asyncEventHandler != null ? asyncEventHandler(sender, args) : Task.CompletedTask;
     }
