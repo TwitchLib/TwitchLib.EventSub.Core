13dcde7a6504af1d035def951e0ecb18b121f082
AoshiW <ondru@LAPTOP-7K2AEST8>
2025-06-25 13:11:30 +0200

    add `Conduit Shard Disabled` subscription type

---
+++ TwitchLib.EventSub.Core/Models/Conduit/ConduitShardDisabledTransport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TwitchLib.EventSub.Core.Models.Conduit;
+
+public sealed class ConduitShardDisabledTransport
+{
+    /// <summary>
+    /// websocket or webhook
+    /// </summary>
+    public string Method { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Optional. Webhook callback URL.
+    /// Null if <see cref="Method"/> is set to websocket.
+    /// </summary>
+    public string? Callback { get; set; }
+
+    /// <summary>
+    /// Optional. WebSocket session ID.
+    /// Null if  <see cref="Method"/> is set to webhook.
+    /// </summary>
+    public string? SessionId { get; set; }
+
+    /// <summary>
+    /// Optional. Time that the WebSocket session connected.
+    /// Null if <see cref="Method"/> is set to webhook.
+    /// </summary>
+    public DateTimeOffset? ConnectedAt { get; set; }
+
+    /// <summary>
+    /// Optional. Time that the WebSocket session disconnected.
+    /// Null if <see cref="Method"/> is set to webhook.
+    /// </summary>
+    public DateTimeOffset? DisconnectedAt { get; set; }
+}
