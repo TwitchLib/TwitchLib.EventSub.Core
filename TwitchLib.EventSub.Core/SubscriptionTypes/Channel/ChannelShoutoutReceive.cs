6b7fe6535c9a2b8f2e949fbdd65898a942e66caa
AoshiW <ondru@LAPTOP-7K2AEST8>
2025-07-11 12:14:55 +0200

    remove deprecated classes/properties

--- TwitchLib.EventSub.Core/SubscriptionTypes/Channel/ChannelShoutoutReceive.cs
+++ TwitchLib.EventSub.Core/SubscriptionTypes/Channel/ChannelShoutoutReceive.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TwitchLib.EventSub.Core.Models.Shoutouts;
+﻿using TwitchLib.EventSub.Core.Models.Shoutouts;
 
 namespace TwitchLib.EventSub.Core.SubscriptionTypes.Channel;
 
@@ -10,21 +9,6 @@ namespace TwitchLib.EventSub.Core.SubscriptionTypes.Channel;
 /// </summary>
 public sealed class ChannelShoutoutReceive : ShoutoutBase
 {
-    /// <summary>
-    /// An ID that identifies the broadcaster that received the Shoutout.
-    /// </summary>
-    [Obsolete("This property is a typo, please use: FromBroadcasterUserId")]
-    public string ToBroadcasterUserId { get; set; } = string.Empty;
-    /// <summary>
-    /// The receiving broadcaster’s display name.
-    /// </summary>
-    [Obsolete("This property is a typo, please use: FromBroadcasterUserName")]
-    public string ToBroadcasterUserName { get; set; } = string.Empty;
-    /// <summary>
-    /// The receiving broadcaster’s login name.
-    /// </summary>
-    [Obsolete("This property is a typo, please use: FromBroadcasterUserLogin")]
-    public string ToBroadcasterUserLogin { get; set; } = string.Empty;
     /// <summary>
     /// An ID that identifies the broadcaster that sent the Shoutout.
     /// </summary>
